f7f1f8e6124527fb4d1cd4c98c527ce9b90a70a2
Aman Chauhan <aman.chauhan@smartenergywater.in>
2024-03-01 16:07:57 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import styled, { ThemeProvider } from 'styled-components';
 import './App.css';
+import { darkTheme } from './theme/theme';
+import Navbar from './component/Navbar';
+import HeroSection from './component/HeroSection';
+import Skills from './component/Skills';
+import Education from './component/Education';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Education />
+          </Wrapper>
+        </Body>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/component/HeroSection/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  HeroBg,
+  HeroContainer,
+  HeroInnerContainer,
+  HeroLeftContainer,
+  HeroRightContainer,
+  Img,
+  ResumeButton,
+  Span,
+  SubTitle,
+  TextLoop,
+  Title,
+} from './HeroStyle';
+import { Bio } from '../../data/constant';
+import Typewriter from 'typewriter-effect';
+import HeroImg from '../../images/aman_pic.JPG'
+import HeroBgAnimation from '../HeroBgAnimation';
+
+const HeroSection = () => {
+  return (
+    <div id='about'>
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer>
+            <Title>
+              Hi, I am <br />
+              {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target='display'>
+              Check Resume
+            </ResumeButton>
+          </HeroLeftContainer>
+          <HeroRightContainer>
+            <Img src={HeroImg} alt="hero-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
+
+export default HeroSection;
